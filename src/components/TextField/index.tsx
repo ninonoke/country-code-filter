955a314d76b7cb28f8c1799ae6deb411df492ff7
Nino Gelovani <ninigelov@gmail.com>
2022-11-21 01:53:38 +0200

    chore: add text field component

---
+++ src/components/TextField/index.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from "react";
+import styles from "./textField.module.scss";
+
+interface TextFieldProps {
+  onChange: (value: string) => void;
+  placeholder?: string;
+  value: string;
+}
+
+const TextField: FC<TextFieldProps> = ({ onChange, placeholder, value }) => {
+  return (
+    <input
+      className={styles.textField}
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default TextField;
