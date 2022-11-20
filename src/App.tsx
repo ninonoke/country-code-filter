bdff5015e137abd128b0e9bde912a42863ec0715
Nino Gelovani <ninigelov@gmail.com>
2022-11-21 01:25:29 +0200

    chore: add eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react'
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
      {}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
