7329f23d2b84811d4bad1141389f97269d501cb0
Nino Gelovani <ninigelov@gmail.com>
2022-11-21 01:50:53 +0200

    chore: add normalizing css

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { apolloClient } from "./apollo";
 import App from "./App";
+import "./styles/normalize.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
